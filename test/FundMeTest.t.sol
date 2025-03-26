803cf9cf5894cbefaf1c79d7a464f7c0c8784a9a
divine <divinemensah102@gmail.com>
2025-03-26 00:34:55 -0400

    chore: forge init

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,14 +4,18 @@ pragma solidity ^0.8.18;
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 
-contract FundMeTest {
+contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        FundMe fundMe = new FundMe();
+        fundMe = new FundMe();
     }
 
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
     }
 }
