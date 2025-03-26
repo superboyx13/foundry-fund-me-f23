70d0a0149f87c56b9bb059b32aa5ab00a2bd7498
divine <divinemensah102@gmail.com>
2025-03-26 00:07:17 -0400

    chore: forge init

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,17 @@
+//SPX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest {
+    FundMe fundMe;
+
+    function setUp() external {
+        FundMe fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+}
